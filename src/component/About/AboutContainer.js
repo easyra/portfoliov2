021dee1a80aeeacbd5f3954c475cad9746810b68
ezra davis davis <ezra.rr.davis@gmail.com>
2019-04-07 15:32:31 -0700

    changed About Layout

--- src/component/About/AboutContainer.js
+++ src/component/About/AboutContainer.js
@@ -1,22 +1,40 @@
 import React, { Component } from 'react';
 import { ProfilePic, ArrowIcons } from '../../reducer';
+import { withRouter } from 'react-router-dom';
 
 class AboutContainer extends Component {
+  handleContact = () => {
+    this.props.history.push('/contact');
+  };
+  handleProjects = () => {
+    this.props.history.push('/projects');
+  };
   render() {
     const { pages, pagesHash } = this.props;
     return (
       <>
         <section class='about'>
           <div className='about-content'>
-            <div>
-              <h1>Ezra Davis</h1>
-              <p>
-                I have been a problem solver ever since I’ve been able to type
-                on a keyboard. Hope we can work together and make something
-                great!
-              </p>
-            </div>
             <ProfilePic />
+            <h1>Ezra Davis</h1>
+
+            <div className='about-btns'>
+              <h4 className='btn'>Learn More</h4>
+              <h4 className='btn' onClick={this.handleProjects}>
+                Go To Projects
+              </h4>
+              <h4 className='btn' onClick={this.handleContact}>
+                Contact Me!
+              </h4>
+            </div>
+            <div className='about-links'>
+              <a href='https://github.com/easyra' target='_blank'>
+                <i class='fab fa-github fa-2x' />
+              </a>
+              <a href='https://www.linkedin.com/in/ezra-davis/' target='_blank'>
+                <i class='fab fa-linkedin fa-2x' />
+              </a>
+            </div>
           </div>
         </section>
         <ArrowIcons title='about' pages={pages} pagesHash={pagesHash} />
@@ -25,4 +43,4 @@ class AboutContainer extends Component {
   }
 }
 
-export default AboutContainer;
+export default withRouter(AboutContainer);
