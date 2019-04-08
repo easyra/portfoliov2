b8dd298534570643c34d1122f0c3331c18ac1faa
ezra davis davis <ezra.rr.davis@gmail.com>
2019-04-08 16:32:49 -0700

    profile pic layout fixed

--- src/component/reuseable/ProfilePic.js
+++ src/component/reuseable/ProfilePic.js
@@ -6,7 +6,11 @@ const ProfilePic = () => {
     'https://pbs.twimg.com/profile_images/1111520035514642432/CXhAHy-X_400x400.png';
   // 'https://media.licdn.com/dms/image/C5603AQHcTgDMY8JZZg/profile-displayphoto-shrink_200_200/0?e=1559779200&v=beta&t=tVwE7W4YQKUR2clFqjmfHxuG7yAA1PSzUnP0bYo-Yqs';
 
-  return <img className='profilepic' src={src} />;
+  return (
+    <div className='profilepic'>
+      <img className='' src={src} />
+    </div>
+  );
 };
 
 export default ProfilePic;
